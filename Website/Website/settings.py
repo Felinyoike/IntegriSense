aae38ab303c643076eba128a15f9100821e4719d
Shemira01 <145838195+Shemira01@users.noreply.github.com>
2025-06-19 11:34:31 +0300

    slight changes

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -121,7 +121,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'WebApp/static'),  # Your app's static files
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (user-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
